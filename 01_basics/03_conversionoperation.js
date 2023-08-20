b9eed29f0af1c7e174555ea754f0ead5c84ed7a2
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-08-20 05:18:56 +0000

    comparision

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -11,12 +11,52 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Paritosh"
 
 let BooleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanIsLoggedIn);
+//console.log(BooleanIsLoggedIn);
 
 //1 => true ; 0 => false
 //"" false
 // "Paritosh" => true
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// *******************operation***************//
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
