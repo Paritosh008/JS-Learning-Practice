cb4b1062870a6557b9d7817f32d5439a6c86a3e4
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-08-20 04:35:34 +0000

    conversionof datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = "Paritosh"
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber)
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1, false => 0
+let isLoggedIn = "Paritosh"
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);
+
+//1 => true ; 0 => false
+//"" false
+// "Paritosh" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
